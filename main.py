274f98feb10bae4badee42a01951688d1ce402c2
Pedro Oria <pedrooria23@gmail.com>
2023-06-12 11:54:03 -0500

    update Ml

--- main.py
+++ main.py
@@ -165,18 +165,18 @@ async def get_director(nombre_director:str):
     }
 
 # funcion de Machine Learnig para recomendarte peliculas
-#@app.get('/recomendacion/{titulo}')
-#async def get_recomendations(title:str):
-    #i = pd.read_csv('recomendacion.csv')
-    #tfidf = TfidfVectorizer(stop_words='english')
-    #i['overview'] = i["overview"].fillna("")
-    #tfidf_matrix = tfidf.fit_transform(i['overview'])
-    #coseno_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
-    #indices = pd.Series(i.index, index=i['title']).drop_duplicates()
-    #idx = indices[title]
-    #simil = list(enumerate(coseno_sim[idx]))
-    #simil = sorted(simil, key=lambda x:x[1], reverse=True)
-    #simil = simil[1:11]
-    #movie_indices = [i[0] for i in simil]
-    #lista = i['title'].iloc[movie_indices].to_list()[:5]
-    #return {'lista recomendada': lista}
+@app.get('/recomendacion/{titulo}')
+async def get_recomendations(titulo:str):
+    i = pd.read_csv('calificadas.csv')
+    tfidf = TfidfVectorizer(stop_words='english')
+    i['overview'] = i["overview"].fillna("")
+    tfidf_matrix = tfidf.fit_transform(i['overview'])
+    coseno_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+    indices = pd.Series(i.index, index=i['title'].str.lower()).drop_duplicates()
+    idx = indices[titulo.lower()]
+    simil = list(enumerate(coseno_sim[idx]))
+    simil = sorted(simil, key=lambda x:x[1], reverse=True)
+    simil = simil[1:11]
+    movie_indices = [i[0] for i in simil]
+    lista = i['title'].iloc[movie_indices].to_list()[:5]
+    return {'lista recomendada': lista}
